cf9991cd09bc54abcf410904f243ea3f71214991
Yuriy <ychokov@gmail.com>
2026-08-18 07:57:17 +0300

    chore(playlist): improve accessibility, use stable key and explicit START_TIME (preserve API)

--- src/components/playlist/PlayList.vue
+++ src/components/playlist/PlayList.vue
@@ -1,53 +1,43 @@
 <template>
-
-<ul class="play-list">
-
+  <ul class="play-list">
     <li
-        v-for="(track,index) in playlist"
-        :key="index"
-        class="track"
-        :class="{
-            selected: index === currentTrack
-        }"
-        @click="selectTrack(index)"
+      v-for="(track, index) in playlist"
+      :key="track.id ?? track.title ?? index"
+      class="track"
+      :class="{ selected: index === currentTrack }"
+      role="button"
+      tabindex="0"
+      :aria-selected="index === currentTrack"
+      @click="selectTrack(index)"
+      @keydown.enter.prevent="selectTrack(index)"
+      @keydown.space.prevent="selectTrack(index)"
     >
-
-        {{ track.title }}
-
+      {{ track.title || 'Untitled' }}
     </li>
-
-</ul>
-
+  </ul>
 </template>
-<style scoped>
-	@import '@/assets/css/play-list.css';
-</style>
 
 <script setup>
+const props = defineProps({
+  playlist: {
+    type: Array,
+    default: () => []
+  },
+  currentTrack: {
+    type: Number,
+    default: 0
+  }
+})
 
-	const props = defineProps({
+const emit = defineEmits(['select'])
 
-   		 playlist:{
-        		type:Array,
-        		default:()=>[]
-    		},
+const START_TIME = 0 // явно: начать трек с начала
 
-    		currentTrack:{
-        		type:Number,
-        		default:0
-    		}
-
-	})
-
-
-	const emit = defineEmits([
-    		'select'
-	])
-
-
-	function selectTrack(index){
-
-    		emit('select', index, 0)
-	}
-
+function selectTrack(index) {
+  emit('select', index, START_TIME)
+}
 </script>
+
+<style scoped>
+@import '@/assets/css/play-list.css';
+</style>
